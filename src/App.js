c15ebc0cd1a1bca11c2ae1f38dfecd5368948d22
Ashraf Azemi <ashraf.chzm@gmail.com>
2021-06-05 14:00:39 +0800

    3rd commit

--- src/App.js
+++ src/App.js
@@ -5,10 +5,21 @@ function App() {
     <>
       <main id="main">
         <header id="header">
-          <h1>header</h1>
+          <div>
+            <h1 id="title">Ashraf Azemi</h1>
+          </div>
+          <div class="nav">
+            <h1 class="nav-child">Profile</h1>
+            <h1 class="nav-child">Project</h1>
+            <h1 class="nav-child">Contact</h1>
+          </div>
         </header>
         <section id="profile">
-          <h1>profile</h1>
+          <h1 id="profile-title">Hey there! I am Ashraf</h1>
+          <img
+            src="https://scontent.fkul8-1.fna.fbcdn.net/v/t31.18172-8/13161912_10153859317843110_6677943647445087117_o.jpg?_nc_cat=106&ccb=1-3&_nc_sid=730e14&_nc_eui2=AeH3JXvZCM9BwYa7yXHXjgIX0iJxIxx6A8rSInEjHHoDypYtvD7HeFTZ5cobaWfC410&_nc_ohc=_iom4H810RMAX_b2aZN&_nc_ht=scontent.fkul8-1.fna&oh=4e02593c8b63b686a85be591e0e685fd&oe=60E1A62D"
+            alt="ashraf"
+          />
         </section>
         <section id="project">
           <h1>project</h1>
