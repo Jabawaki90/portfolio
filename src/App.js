03ec00a476cae5ed34dccb949808f6b1feefa48b
Ashraf Azemi <ashraf.chzm@gmail.com>
2021-06-06 11:47:11 +0800

    update welcome & project id

--- src/App.js
+++ src/App.js
@@ -14,14 +14,14 @@ function App() {
             <h1 class="nav-child">Contact</h1>
           </div>
         </header>
-        <section id="profile">
+        <section id="welcome-section">
           <h1 id="profile-title">Hey there! I am Ashraf</h1>
           <img
             src="https://scontent.fkul8-1.fna.fbcdn.net/v/t31.18172-8/13161912_10153859317843110_6677943647445087117_o.jpg?_nc_cat=106&ccb=1-3&_nc_sid=730e14&_nc_eui2=AeH3JXvZCM9BwYa7yXHXjgIX0iJxIxx6A8rSInEjHHoDypYtvD7HeFTZ5cobaWfC410&_nc_ohc=_iom4H810RMAX_b2aZN&_nc_ht=scontent.fkul8-1.fna&oh=4e02593c8b63b686a85be591e0e685fd&oe=60E1A62D"
             alt="ashraf"
           />
         </section>
-        <section id="project">
+        <section id="projects">
           <h1>project</h1>
         </section>
         <section id="contact">
