cc6b1b73402248c6e1199050ee226e71982a4a8f
Ashraf Azemi <ashraf.chzm@gmail.com>
2021-06-09 20:24:05 +0800

    Portfolio Landing Page completed

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <main id="main">
-        <header id="header">
+        <nav id="header">
           <div>
             <h1 id="title">Ashraf Azemi</h1>
           </div>
@@ -21,7 +21,7 @@ function App() {
               <h1 class="nav-child">Contact</h1>
             </a>
           </div>
-        </header>
+        </nav>
         <section id="welcome-section">
           <h1 id="profile-title">Hey there! I am Ashraf</h1>
           <h3 id="profile-quote">a self-taught web developer</h3>
@@ -33,7 +33,7 @@ function App() {
         </section>
         <section id="projects">
           <h1 id="portfolio-title">Portfolio</h1>
-          <div id="portfolio">
+          <div id="portfolio" className="project-tile">
             <section class="img-container">
               <figure>
                 <a href="https://codepen.io/jabawaki90/pen/abJVQLv">
@@ -48,31 +48,37 @@ function App() {
             </section>
             <section class="img-container">
               <figure>
-                <img
-                  class="img-port"
-                  src={process.env.PUBLIC_URL + "/image/img2.png"}
-                  alt=""
-                />
+                <a href="https://codepen.io/jabawaki90/pen/mdWqpyG">
+                  <img
+                    class="img-port"
+                    src={process.env.PUBLIC_URL + "/image/img2.png"}
+                    alt=""
+                  />
+                </a>
                 <figcaption>Tribute Page</figcaption>
               </figure>
             </section>
             <section class="img-container">
               <figure>
-                <img
-                  class="img-port"
-                  src={process.env.PUBLIC_URL + "/image/img3.png"}
-                  alt=""
-                />
+                <a href="https://codepen.io/jabawaki90/pen/oNZpeWp">
+                  <img
+                    class="img-port"
+                    src={process.env.PUBLIC_URL + "/image/img3.png"}
+                    alt=""
+                  />
+                </a>
                 <figcaption>Landing Page</figcaption>
               </figure>
             </section>
             <section class="img-container">
               <figure>
-                <img
-                  class="img-port"
-                  src={process.env.PUBLIC_URL + "/image/img4.png"}
-                  alt=""
-                />
+                <a href="https://codepen.io/jabawaki90/pen/dyvdaEE">
+                  <img
+                    class="img-port"
+                    src={process.env.PUBLIC_URL + "/image/img4.png"}
+                    alt=""
+                  />
+                </a>
                 <figcaption>Documentation</figcaption>
               </figure>
             </section>
@@ -83,45 +89,45 @@ function App() {
           <h6 id="quote">
             "Alone we can do so little; together we can do so much."
           </h6>
-          <div className="icon">
-            <section id="linkedin" class="social-media">
-              <FaLinkedin class="icon " />
+          <div className="icon-container">
+            <section id="linkedin" className="social-media">
+              <FaLinkedin className="icon" />
               <a href="https://www.linkedin.com/in/ashrafazemi/">
-                <h3 class="label">LinkedIn</h3>
+                <h3 className="label">LinkedIn</h3>
               </a>
             </section>
-            <section id="facebook" class="social-media">
-              <FaFacebook class="icon " />
+            <section id="facebook" className="social-media">
+              <FaFacebook className="icon" />
               <a href="https://www.facebook.com/ashraf.azemi/">
-                <h3 class="label">Facebook</h3>
+                <h3 className="label">Facebook</h3>
               </a>
             </section>
-            <section id="github" class="social-media">
-              <FaGithub class="icon " />
+            <section id="github" className="social-media">
+              <FaGithub className="icon" />
               <a href="https://github.com/Jabawaki90">
-                <h3 class="label">Github</h3>
+                <h3 className="label">Github</h3>
               </a>
             </section>
-            <section class="social-media">
-              <MdEmail class="icon " />
+            <section className="social-media">
+              <MdEmail className="icon" />
               <a href="#footer-email">
-                <h3 class="label">Email</h3>
+                <h3 className="label">Email</h3>
               </a>
             </section>
-            <section class="social-media">
-              <MdPhoneAndroid class="icon " />
+            <section className="social-media">
+              <MdPhoneAndroid className="icon" />
               <a href="#footer-tel">
-                <h3 class="label">Call me</h3>
+                <h3 className="label">Call me</h3>
               </a>
             </section>
           </div>
         </section>
         <footer id="footer">
           <div className="left-side">
-            <h3 id="footer-email" class="child">
+            <h3 id="footer-email" className="child">
               ashraf.chzm@gmail.com
             </h3>
-            <h3 id="footer-tel" class="child">
+            <h3 id="footer-tel" className="child">
               +6016 201 9424
             </h3>
           </div>
